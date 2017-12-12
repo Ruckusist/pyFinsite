2a1571a07a781a212602adf87c48738a34dce3d1
Eric <eric.alphagriffin@gmail.com>
2017-12-12 03:48:26 -0700

    testing

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import ccxt
+import datetime
 
 print("Content-type: text/html")
-print("<html><head/>")
-print("<body>")
-print("Kinetech Concepts Financial Reports")
+print("<html><head>")
+print("<title>Kinetech Concepts</title>")
+print("</head>")
+print("<body bgcolor=#131116 text=red>")
+print("<h1>Kinetech Concepts Financial Reports</h1>")
 print("<br>")
 print("Available Exchanges:")
 print("<br>")
